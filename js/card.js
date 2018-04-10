53ff65442333fe673c11b405933b907ae002575b
Karol Dąbrowski <dabrowski.carl@gmail.com>
2018-04-10 19:02:32 +0200

    add gitignore, delete node

--- js/card.js
+++ js/card.js
@@ -1,24 +1,22 @@
 // KLASA KANBAN CARD
-function Card(id, name) {
-    var self = this;
-    this.id = id;
-    this.name = name || 'No name given';
-    this.element = createCard();
-
-    function createCard() {
-        var card = $('<li class="card"></li>');
-        var cardDeleteBtn = $('<button class="btn-delete">x</button>');
-        var cardDescription = $('<p class="card-description"></p>');
-
-        cardDeleteBtn.click(function () {
-            self.removeCard();
-        });
-
-
-        card.append(cardDeleteBtn);
-        cardDescription.text(self.name);
-        card.append(cardDescription)
-        return card;
+class Card {
+    constructor(id, name) {
+        var self = this;
+        this.id = id;
+        this.name = name || 'No name given';
+        this.element = createCard();
+        function createCard() {
+            var card = $('<li class="card"></li>');
+            var cardDeleteBtn = $('<button class="btn-delete">x</button>');
+            var cardDescription = $('<p class="card-description"></p>');
+            cardDeleteBtn.click(function () {
+                self.removeCard();
+            });
+            card.append(cardDeleteBtn);
+            cardDescription.text(self.name);
+            card.append(cardDescription);
+            return card;
+        }
     }
 }
 Card.prototype = {
